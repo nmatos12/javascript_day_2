2874024a72293058ea1b3fbd308c3f4709763d4e
unknown <nataliematos12@gmail.com>
2023-03-08 14:33:06 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+
+// var color = 'orange';
+// var num = 10;
+// var stringNum = '10'
+// var myNameIsNatalie = true;
+// var thisClassIsEasy = false;
+
+// console.log(color);
+
+// // console.log(num==color);
+// // var theSum = num === stringNum;
+// // var greaterThan = num > 5;
+// //console.log(greaterThan);
+
+// var firstName = 'natalie';
+
+// console.log(firstName.includes('n'));
+
+
+// var zero = 0; //only number thats falsy all above and below are truthy
+// var num = 50; //truthy
+// var empty = ''; //empty string will give you falsy, however as long as there is a space or letters it will be truthy
+// var str = 'Some String' //truthy
+// var un = undefined; //falsy
+// var f = false; //falsy
+// var t = true; //truthy
+// var n = null; //falsy
+// var and = 100 && true;
+
+// var age = 17
+// var isOldEnough = age >= 18 && true;
+
+// console.log(isOldEnough);
+
+// var old = age > 99;
+
+// console.log(!old)
+
+var age = 18;
+if (age >= 21) {
+    console.log('Is old enough');
+} else if (age >= 18) {
+    console.log('Old enough to party')
+} else if (age < 18 && age > 12) {
+    console.log('You are a brat')
+} else {
+    console.log('Not old enough');
+}
